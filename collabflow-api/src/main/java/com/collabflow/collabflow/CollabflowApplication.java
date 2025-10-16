0b823e41345a12be4d49be84f630d82f0c957abf
KaizerX01 <tshensowgamer2004@gmail.com>
2025-10-16 19:07:46 +0100

    chore: add initial backend and frontend folders

---
+++ collabflow-api/src/main/java/com/collabflow/collabflow/CollabflowApplication.java
@@ -0,0 +1,13 @@
+package com.collabflow.collabflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabflowApplication.class, args);
+    }
+
+}
---
+++ collabflow-api/src/test/java/com/collabflow/collabflow/CollabflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabflow.collabflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
